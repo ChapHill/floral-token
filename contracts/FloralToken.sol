68cf15f5e151d5c9e3ee1008399427961969ee90
Clay Carroll <claycarroll7@gmail.com>
2021-10-03 15:59:35 -0500

    feat(contracts/FloralToken): added more functionality to the token contract

--- contracts/FloralToken.sol
+++ contracts/FloralToken.sol
@@ -12,8 +12,11 @@ contract FloralToken {
     //must trigger when tokens are transferred
     event Transfer(address indexed _from, address indexed _to, uint _value);
 
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+
     //key value pairing
-    mapping(address => uint) public balanceOf;
+    mapping(address => uint) public balanceOf; //returns amount of tokens owned by account
+    mapping(address => mapping(address => uint)) public allowance; //returns remaining # of tokens that spender will be allowed to spend on behalf of owner
 
     //constructor
     //set the total supply to the initial supply (in ./migrations)
@@ -22,12 +25,35 @@ contract FloralToken {
         totalSupply = _initialSupply;
     }
 
+    function approve(address _spender, uint _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    //transfer tokens from one account to another
     function transfer(address _to, uint _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value); //if user doesn't have enough
         balanceOf[msg.sender] -= _value; //subtracts balance from sender
         balanceOf[_to] += _value; //adds to balance to recipient
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+
+    //moves the amount of tokens from sender to recipient using the allowance mechanism
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
--- test/FloralToken.js
+++ test/FloralToken.js
@@ -13,15 +13,34 @@ contract("FloralToken test", async accounts => {
         assert.equal(balance.toNumber(), 500000);
     });
 
+    it("should transfer 1000 tokens from the first account to the second account", async () => {
+        const instance = await FloralToken.deployed();
+        await instance.transfer(accounts[1], 1000, {from: accounts[0]});
+        const balance = await instance.balanceOf(accounts[1])
+        assert.equal(balance.toNumber(), 1000);
+    });
+    
+    // it("should 50000 tokens in the first account", async () => {
+    //     const instance = await FloralToken.deployed();
+    //     const balance = await instance.balanceOf(accounts[0]);
+    //     assert.equal(balance.toNumber(), 50000);
+    // });
+
+    // it("should put 100 tokens in the second account", async () => {
+    //     const instance = await FloralToken.deployed();
+    //     const balance = await instance.balanceOf(accounts[1]);
+    //     assert.equal(balance.toNumber(), 100);
+    // });
+
+    // it("should put 2000 tokens in the third account", async () => {
+    //     const instance = await FloralToken.deployed();
+    //     const balance = await instance.balanceOf(accounts[2]);
+    //     assert.equal(balance.toNumber(), 500000);
+    // });
+
     it("should have the name Floral Token", async() => {
         const instance = await FloralToken.deployed();
         const name = await instance.tokenName();
         assert.equal(name, "Floral Token")
     });
-
-    it("should transfer tokens successfully"), async() => {
-        const instance = await FloralToken.deployed();
-        const test = await instance.transfer.call(accounts[1], 999999999999);
-        assert.fail(test, "accounts does not have enough tokens")
-    }
 });
