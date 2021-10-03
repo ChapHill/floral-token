c7a2eb1222444322f9def11f3ba6b13e2814d50b
Clay Carroll <claycarroll7@gmail.com>
2021-10-03 16:23:35 -0500

    added(contracts/FloralTokenSale): added another contract for buying of tokens

---
+++ contracts/FloralTokenSale.sol
@@ -0,0 +1,8 @@
+//specify version of solidity 
+pragma solidity >=0.4.22 <0.9.0;
+
+contract FloralTokenSale {
+    constructor() public{
+        
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const FloralToken = artifacts.require("./FloralToken.sol");
+const FloralTokenSale = artifacts.require("./FloralTokenSale.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(FloralToken, 500000); //set total supply to 500,000
+  deployer.deploy(FloralTokenSale);
 };
